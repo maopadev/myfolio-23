c71010b6298378cdd2c0b4e1eeef3dcd2b8d8f31
Mario Ortega Palacios <mortegap.ext@ayesa.com>
2023-01-13 12:17:30 +0100

    background > apply noise and animated blur

---
+++ src/components/background.svelte
@@ -0,0 +1,14 @@
+<!-- Background Blur and Noise -->
+<div class="bg-transparent w-screen min-h-screen absolute flex items-center justify-center px-16 -z-10 noise">
+  <div class="relative w-full max-w-full">
+		<div class={`absolute top-0 -right-96 sphereThree bg-[#f7e7dc] rounded-full filter blur-3xl opacity-10 animate-blob animation-delay-2000`} />
+    <div class={`absolute bottom-24 -left-96 sphereOne bg-[#e0f0e9] rounded-full filter blur-3xl opacity-20 animate-blob animation-delay-4000`} />
+  </div>
+</div>
+
+<script lang="js">
+
+import '@styles/background.scss'
+
+</script>
+  
