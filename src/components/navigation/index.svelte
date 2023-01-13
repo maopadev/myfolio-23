344e6100566773785256e24d8083bcbfee81fd02
Mario Ortega Palacios <mortegap.ext@ayesa.com>
2023-01-13 11:08:07 +0100

    navigation > se establece navigation bar

---
+++ src/components/navigation/index.svelte
@@ -0,0 +1,25 @@
+<!-- Navigation Component -->
+<header class="fixed w-full flex top-0 justify-between items-center px-12 p-4 z-20 border-b-2 border-bigstone bg-transparent backdrop-blur-lg backdrop-filter">
+	<div id="corner" class="flex items-center justify-center w-16 h-auto">
+		<a href='/'>
+			<img src={logo} alt="@mrtgdev" />
+		</a>
+	</div>
+	<!-- Information Web -->
+	<div id="corner" class="inline-flex h-12 w-auto gap-8 align-middle items-center">
+		<a class="flex items-center justify-center w-8 h-auto" 
+			href="https://github.com/mrtgdev/my-portfolio">
+			<img src={github} alt="GitHub" />
+		</a>
+	</div>
+</header>
+
+<script lang="js">
+
+import logo from '$lib/assets/images/favicon.webp'
+import github from '$lib/assets/images/github.svg'
+
+// Style SCSS
+import '@style/navigation.scss'
+
+</script>
