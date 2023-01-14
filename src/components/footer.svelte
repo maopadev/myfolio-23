652b7029927f88baad8d283877d6d54393372d6e
mrtgdev <mrtg.dev@outlook.com>
2023-01-14 13:24:31 +0100

    footer > se integra footer de flowbite

---
+++ src/components/footer.svelte
@@ -0,0 +1,20 @@
+
+<!-- Footer Component -->
+<section id="footer" class="flex w-screen justify-between items-center z-10 bg-transparent pb-8 px-12 md:px-20 xl:px-24">
+    <Footer>
+      <FooterCopyright href="/" by="flowbite™" year={2022} />
+      <FooterLinkGroup ulClass="flex flex-wrap items-center justify-center text-sm text-gray-500 dark:text-gray-400">
+        <FooterLink href="/">About</FooterLink>
+        <FooterLink href="/">Privacy Policy</FooterLink>
+        <FooterLink href="/">Licensing</FooterLink>
+        <FooterLink href="/">Contact</FooterLink>
+      </FooterLinkGroup>
+    </Footer>
+</section>
+
+<script lang="js">
+
+// > Components Imports
+import { Footer, FooterCopyright, FooterLinkGroup, FooterLink } from 'flowbite-svelte'
+
+</script>
