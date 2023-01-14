81ee35976b7f2f589b7f1a1d2d8f6ace22516344
mrtgdev <mrtg.dev@outlook.com>
2023-01-14 13:25:13 +0100

    navbar > se corrige la posicion y se agrega un boton para colores de la página

--- src/components/navigation.svelte
+++ src/components/navigation.svelte
@@ -1,7 +1,6 @@
 
 <!-- Navigation Component -->
-
-<header class="fixed w-screen h-12 flex top-6 lg:top-20 justify-between items-center p-4 z-10 bg-transparent px-12 md:px-20 xl:px-24">
+<header class="relative w-screen h-12 flex top-6 lg:top-20 justify-between items-center p-4 z-10 bg-transparent px-12 md:px-20 xl:px-24">
 	<!-- Information -->
 	<div id="corner-left" class="flex items-center justify-center w-auto h-12 gap-24 z-50">
 		<div class="flex items-center justify-center w-12 lg:w-16 h-12 lg:h-16">
@@ -9,11 +8,11 @@
 				<img src={logo} alt="Go home" />
 			</a> 
 		</div>
-		<div class="inline-flex gap-4">
+		<div class="hidden lg:inline-flex gap-4">
 			<p>My Portfolio</p>
 			<p>20{ getCurrentYear() }</p>
 		</div>
-		<div class="inline-flex gap-4"> 
+		<div class="hidden lg:inline-flex gap-4"> 
 			<h1 class=" font-bold">Location</h1>
 			{coordinates}
 		</div>
@@ -24,10 +23,7 @@
 		-> Mostrar el modo oscuro
 	-->
 	<div id="corner-right" class="inline-flex h-12 w-auto gap-8 align-middle items-center ">
-		<button id="dark" class=" flex flex-wrap items-center justify-center w-auto h-8" 
-			on:click={() => console.log('Se pulso el Dark')}>
-			<h1>Dark mode</h1>
-		</button>
+		<DarkMode class="" />
 		<div class="border-2 border-slate rounded-full px-8 p-2">
 			<a id="github-button" class="flex items-center justify-center w-8 h-auto" 
 				href="https://github.com/mrtgdev/my-portfolio">
@@ -42,10 +38,13 @@
 import github from '@images/github.svg'
 import logo from '@images/favicon.webp'
 
+// > Components Import
+import { DarkMode } from 'flowbite-svelte'
+
 // Style SCSS
 import '@styles/navigation.scss'
 
-let coordinates = '';
+let coordinates = ''
 
 const getCurrentYear = (() => {
     let currentTime = new Date()
