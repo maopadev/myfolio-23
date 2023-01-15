5ce774916ac46ea116dfdf079e6291ee6bde9780
mrtgdev <mrtg.dev@outlook.com>
2023-01-15 21:50:03 +0100

    about > se agrega al layout el botón que abrira el dialog modal

--- src/islands/about.svelte
+++ src/islands/about.svelte
@@ -1,5 +1,10 @@
 
 <!-- About Me Island -->
-<div>
-    
-</div>
+<section id="about" class="flex w-full min-h-screen justify-between items-center z-10 px-12 md:px-20 xl:px-24">
+    <button class=""
+        on:click={() => console.log('holi')}>
+        <div>
+            Sobre Mi
+        </div>
+    </button>
+</section>
