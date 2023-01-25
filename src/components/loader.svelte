6d7485d1396ad2ff669980eab386851a8a44ec6c
mrtgdev <mrtg.dev@outlook.com>
2023-01-25 22:34:01 +0100

    loader > se crea componente y se agrega el nombre de la ruta (en proceso)

---
+++ src/components/loader.svelte
@@ -0,0 +1,13 @@
+<div class="load-container">
+    <div class="loading-screen bg-bigstone dark:bg-ocean-green flex w-screen min-h-screen overflow-hidden justify-center items-center">
+        <h1 id="text-loading" class="text-3xl text-bold font-tusker uppercase overstroke">
+            { routeName }
+        </h1>
+    </div>
+</div>
+
+<script lang="js">
+
+let routeName = 'Aqui irá el logo app/proyecto y el nombre del proyecto'
+
+</script>
