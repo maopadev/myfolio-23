dc1fb0b3f49111eae4c733c1c28448b3b9fdb05a
mrtgdev <mrtg.dev@outlook.com>
2023-01-25 22:32:54 +0100

    bg > se ajusta los elementos transformables y sus colores para los distintos temas de la web

--- src/components/background.svelte
+++ src/components/background.svelte
@@ -1,10 +1,10 @@
 <!-- Background Blur and Noise -->
-<div class="w-screen max-h-full absolute flex items-center justify-center -z-10 bg-transparent noise">
+<div class="w-screen min-h-screen flex fixed items-center justify-center -z-10 bg-transparent noise">
   <div class="relative w-full max-w-full max-h-full">
-		<div class="flex absolute top-0 -rigth-28 sphereThree bg-ocean-dark dark:bg-ocean-green rounded-full filter
-      blur-3xl opacity-40 dark:opacity-10 animate-blob animation-delay-2000" />
-    <div class="flex absolute bottom-0 -rigth-36 sphereOne bg-ocean-dark dark:bg-bigstone rounded-full filter
-      blur-3xl opacity-30 dark:opacity-10 animate-blob animation-delay-4000" />
+		<div class="flex absolute top-0 -rigth-28 sphereThree bg-lime dark:bg-ocean-green rounded-full filter
+      blur-3xl opacity-30 dark:opacity-10 animate-blob animation-delay-2000" />
+    <div class="flex absolute bottom-0 -rigth-36 sphereOne bg-tangerine dark:bg-bigstone rounded-full filter
+      blur-3xl opacity-20 dark:opacity-10 animate-blob animation-delay-4000" />
   </div>
 </div>
 
