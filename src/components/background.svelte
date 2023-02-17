64cbeeaee606025ded05d7610895fb034f02f763
mrtgdev <mrtg.dev@outlook.com>
2023-02-17 19:01:14 +0100

    back > nuevo background

--- src/components/background.svelte
+++ src/components/background.svelte
@@ -1,20 +1,15 @@
+
 <!-- Background Blur and Noise -->
-<div class="w-screen min-h-screen flex fixed items-center justify-center -z-10 bg-transparent noise">
-  <div class="container relative w-full max-w-full max-h-full z-0">
-		<div class="lines">
-      <div class="line"></div>
-      <div class="line"></div>
-      <div class="line"></div>
-    </div>    
+
+<div class="container fixed w-screen h-full justify-center items-center">
+  <div class="w-screen min-h-screen absolute top-0 m-0 wrap-perspective noise">
+    <div class="top-plane filter blur-lg" />
+    <div class="bottom-plane filter blur-lg "></div>
   </div>
 </div>
 
 <script lang="js">
 
-import '@styles/background.scss'
+import '@styles/background.scss';
 
 </script>
-
-<style>
-</style>
-  
