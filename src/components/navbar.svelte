f7737423ee32e8a89a55ff654a5652ccdea6290b
mrtgdev <mrtg.dev@outlook.com>
2023-02-17 20:40:40 +0100

    navbar > se ajusta el navbar para version de escritorio

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,12 +1,14 @@
 
 <!-- Navigation Component -->
-<navigation class="fixed flex w-screen top-0 justify-between items-center m-auto p-2 px-12 z-20 
-		bg-transparent border-b-4 border-b-bigstone dark:border-b-black backdrop-blur-xl">
-	<div id="corner-left" class="flex items-center justify-center w-screen h-12 gap-24 z-50">
-		<div class="flex items-center justify-center h-12 lg:h-16 gap-4">
-			<a class="w-12 h-12" href='/'>
-				<img src={ logo } alt="" class="text-xs text-cultured dark:text-bigstone"/>
-			</a>
+<navigation class="fixed w-screen top-0 justify-center items-center m-auto py-4 md:py-8 lg:py-12 px-auto z-10 
+		bg-transparent transition-all duration-150">
+	<div class="flex max-w-screen-xl justify-between items-center m-auto bg-transparent">
+		<div id="corner-left" class="flex items-center justify-center md:justify-start w-full h-12 gap-24 z-10">
+			<div class="flex items-center justify-center h-12 lg:h-16 gap-4 ">
+				<a class="w-12 h-12" href='/'>
+					<img src={ logo } alt="" class="text-xs text-cultured dark:text-bigstone"/>
+				</a>
+			</div>
 		</div>
 	</div>
 </navigation>
